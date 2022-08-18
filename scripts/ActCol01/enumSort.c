2809a283b3d46feeb8018eda16708a2492c202c8
MartinNoboa <nomonoboa@gmail.com>
2022-08-18 18:11:12 -0500

    Add script folder w/ 1st collab activity

---
+++ scripts/ActCol01/enumSort.c
@@ -0,0 +1,16 @@
+// =================================================================
+//
+// File: enumSort.c
+// Author: Martin Noboa - A01704052
+// Description: This program implements the enumeration sort algorithm
+//
+// =================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "utils.h"
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
---
+++ scripts/ActCol01/piApprox.c
@@ -0,0 +1,17 @@
+// =================================================================
+//
+// File: piApprox.c
+// Author: Martin Noboa - A01704052
+// Description: This program implements the Leibniz algorithm for 
+// 				PI approximation
+//
+// =================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "utils.h"
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
